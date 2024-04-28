b8ca567e9fd015c6eb0ff251e32672c1f125cf45
unknown <you@example.com>
2024-04-28 18:23:28 +0530

    userId

--- app.js
+++ app.js
@@ -12,7 +12,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 const sequelize = require('./models/index');
-const Users = require('./models/index')
+const Users = require('./models/Users')
+const Expense = require('./models/expenses')
+
+
+Users.hasMany(Expense);
+Expense.belongsTo(Users);
 
 app.use('/signup',signuproutes);
 app.use('/login' , loginroutes);
